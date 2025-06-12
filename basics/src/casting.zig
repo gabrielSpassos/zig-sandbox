bf1dfa89d9f5ee420fcc62e12ff2683be498fe00
Gabriel Passos <gabrielsantos45725@gmail.com>
2025-06-12 19:58:32 -0300

    fix casting

--- basics/src/casting.zig
+++ basics/src/casting.zig
@@ -4,10 +4,9 @@ const print = std.debug.print;
 pub fn main() !void {
     print("Casting sample.\n", .{});
 
-    var x: i32 = 42;
+    const x: i32 = 42;
     print("x: {}\n", .{x});
 
-    const y = @intCast(x);
+    const y: u8 = @intCast(x);
     print("cast x: {}\n", .{y});
-    
 }
